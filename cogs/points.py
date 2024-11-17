fd0892ba9dfcfde2c6450d9228715ea4abd74d8d
TheAihopGG <108916224+TheAihopGG@users.noreply.github.com>
2024-11-17 20:09:04 +0700

    Initial

--- cogs/points.py
+++ cogs/points.py
@@ -2,16 +2,16 @@ import disnake
 from disnake.ext import commands
 from services.interfaces import PointsCogAdminInterface, PointsCogMemberInterface
 
-class AdminCog(commands.Cog, PointsCogAdminInterface):
+class PointsAdminCog(commands.Cog, PointsCogAdminInterface):
     def __init__(self, bot: commands.Bot):
         super().__init__()
         self.bot = bot
 
-class MemberCog(commands.Cog, PointsCogMemberInterface):
+class PointsMemberCog(commands.Cog, PointsCogMemberInterface):
     def __init__(self, bot: commands.Bot):
         super().__init__()
         self.bot = bot
 
 def setup(bot: commands.Bot):
-    bot.add_cog(AdminCog())
-    bot.add_cog(MemberCog())
+    bot.add_cog(PointsAdminCog(bot))
+    bot.add_cog(PointsMemberCog(bot))
--- cogs/ranks.py
+++ cogs/ranks.py
@@ -2,16 +2,16 @@ import disnake
 from disnake.ext import commands
 from services.interfaces import RanksCogAdminInterface, RanksCogMemberInterface
 
-class AdminCog(commands.Cog, RanksCogAdminInterface):
+class RanksAdminCog(commands.Cog, RanksCogAdminInterface):
     def __init__(self, bot: commands.Bot):
         super().__init__()
         self.bot = bot
 
-class MemberCog(commands.Cog, RanksCogMemberInterface):
+class RanksMemberCog(commands.Cog, RanksCogMemberInterface):
     def __init__(self, bot: commands.Bot):
         super().__init__()
         self.bot = bot
 
 def setup(bot: commands.Bot):
-    bot.add_cog(AdminCog())
-    bot.add_cog(MemberCog())
+    bot.add_cog(RanksAdminCog(bot))
+    bot.add_cog(RanksMemberCog(bot))
