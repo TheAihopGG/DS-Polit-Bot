9213839e55c65a62bbc339c68e133aceb883df73
TheAihopGG <108916224+TheAihopGG@users.noreply.github.com>
2024-11-17 19:25:54 +0700

    Added interfaces definitions

---
+++ services/interfaces.py
@@ -0,0 +1,13 @@
+import disnake
+
+class PointsCogAdminInterface(): pass
+
+class PointsCogMemberInterface(): pass
+
+class JobsCogAdminInterface(): pass
+
+class JobsCogMemberInterface(): pass
+
+class RanksCogAdminInterface(): pass
+
+class RanksCogMemberInterface(): pass
