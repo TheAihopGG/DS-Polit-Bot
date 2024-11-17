9425d1ac95a66a6b0d1896f2b89eab7a023d8c4d
TheAihopGG <108916224+TheAihopGG@users.noreply.github.com>
2024-11-17 20:52:05 +0700

    Added interfaces by утка

--- services/interfaces.py
+++ services/interfaces.py
@@ -1,13 +1,59 @@
-import disnake
+class PointsCogAdminInterface():
+    def give_points(self, inter, whom, quantity): pass
 
-class PointsCogAdminInterface(): pass
+    def take_points(self, inter, whom, quantity): pass
 
-class PointsCogMemberInterface(): pass
+    def zero_points(self, inter, whom): pass
 
-class JobsCogAdminInterface(): pass
+    def zero_all_points(self, inter): pass
 
-class JobsCogMemberInterface(): pass
+    def off_points(self, inter): pass
 
-class RanksCogAdminInterface(): pass
+    def on_points(self, inter): pass
 
-class RanksCogMemberInterface(): pass
+class PointsCogMemberInterface():
+    def points_info(self, inter): pass
+
+    def points_top(self, inter): pass
+
+class JobsCogAdminInterface():
+    def give_job(self, inter, member, role): pass
+
+    def take_job(self, inter, member, role): pass
+
+    def add_job(self, inter, role, topic): pass
+
+    def remove_job(self, inter, role): pass
+
+    def edit_job(self, inter, role, topic): pass
+
+class JobsCogMemberInterface():
+    def job_list(self, inter): pass
+
+    def refuse_job(self, inter): pass
+
+class RanksCogAdminInterface():
+    def add_rank(self, inter, role, points): pass
+
+    def remove_rank(self, inter, role): pass
+
+    def edit_rank(self, inter, role, points): pass
+
+class RanksCogMemberInterface():
+    def ranks(self, inter): pass
+
+    def rank(self, inter, member): pass
+
+class CommonCogAdminInterface():
+    def remove_member(self, inter, member): pass
+
+    def add_member(self, inter, member): pass
+
+    def edit_town_topic(self, inter, town, topic): pass
+
+    def town_member_role(self, inter, role): pass
+
+class CommonCogMemberInterface():
+    def members_list(self, inter): pass
+
+    def town_topic(self, inter): pass
