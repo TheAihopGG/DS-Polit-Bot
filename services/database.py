43a77377a448c7faa9eaefbf9d3539992bf4760c
TheAihopGG <108916224+TheAihopGG@users.noreply.github.com>
2024-11-17 20:09:11 +0700

    Initial

---
+++ services/database.py
@@ -0,0 +1,33 @@
+import aiosqlite
+from data.settings import DB_PATH
+
+async def create_tables():
+    async with aiosqlite.connect(DB_PATH) as db:
+        await db.execute('''
+            CREATE TABLE IF NOT EXISTS jobs (
+                job_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                guild_id INTEGER
+            );
+        ''')
+
+        await db.execute('''
+            CREATE TABLE IF NOT EXISTS ranks (
+                rank_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                points_required INTEGER NOT NULL,
+                guild_id INTEGER
+            );
+        ''')
+
+        await db.execute('''
+            CREATE TABLE IF NOT EXISTS users (
+                user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                guild_id INTEGER,
+                username TEXT NOT NULL,
+                points INTEGER DEFAULT 0,
+                rank_id INTEGER,
+                roles JSON DEFAULT [], 
+                FOREIGN KEY (rank_id) REFERENCES ranks(rank_id)
+            );
+        ''')
+
+        await db.commit()
