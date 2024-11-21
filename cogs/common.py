40b081d09642bcabf8b74fb9e85dbba5af49eb9d
qwilton-god <antoshakopakov@gmail.com>
2024-11-21 16:09:07 +0300

    павп
    пап

--- cogs/common.py
+++ cogs/common.py
@@ -28,11 +28,11 @@ class CommonAdminCog(commands.Cog, CommonCogAdminInterface):
             ''', (inter.guild_id, None , inter.guild.name, "-"))
 
             await db.commit()
-            await inter.response.send_message("Сервер успешно установлен в базу данных!\n\n Также используйте /setup_member_role и /edit_town_topic")
+            await inter.response.send_message("Сервер успешно установлен в базу данных!\n\nТакже используйте /set_member_role и /edit_town_topic")
             
-    @commands.slash_command(name='setup_member_role')
+    @commands.slash_command(name='set_member_role')
     @commands.has_permissions(administrator=True)
-    async def setup_member_role(
+    async def set_member_role(
         self,
         inter: disnake.ApplicationCommandInteraction,
         role: disnake.Role
@@ -120,31 +120,39 @@ class CommonMemberCog(commands.Cog, CommonCogMemberInterface):
         super().__init__()
         self.bot = bot
     
-    async def get_town_role_id(self, inter: disnake.ApplicationCommandInteraction):
-        async with aiosqlite.connect(DB_PATH) as db:
-            async with db.execute('''
-                SELECT town_role_id FROM towns WHERE guild_id = ?;
-            ''', (inter.guild_id,)) as cursor:
-                if role := await cursor.fetchone():
-                    return role[0]
-                return None
+
 
     @commands.slash_command(name='member_list')
-    async def members_list(self, inter: disnake.ApplicationCommandInteraction):
-        role_id = await self.get_town_role_id(inter)  # Получаем ID роли города
-        if role_id is None:
-            await inter.response.send_message("Роль города не найдена.", ephemeral=True)
-        
+    async def member_list(self, inter: disnake.ApplicationCommandInteraction):
         async with aiosqlite.connect(DB_PATH) as db:
+            async with db.execute(''' 
+                SELECT town_role_id FROM towns WHERE guild_id = ?; 
+            ''', (inter.guild_id,)) as cursor:
+                if role := await cursor.fetchone():
+                    role_id = role[0]
+                else:
+                    await inter.response.send_message("Роль города не найдена.", ephemeral=True)
+                    return
+
             if members := await (await db.execute('''
                 SELECT user_id FROM users
-                WHERE town_id = ?
+                WHERE town_id = ? 
             ''', (inter.guild_id,))).fetchall():
                 member_names = [f'<@{user_id}>' if any(role.id == role_id for role in (await inter.guild.fetch_member(user_id)).roles) else None for user_id in [member[0] for member in members]]
                 await inter.response.send_message("Список пользователей:\n" + "\n".join(member_names))
             else:
                 await inter.response.send_message("В базе данных нет пользователей для этой гильдии.")
-
+    @commands.slash_command(name='town_topic')
+    async def town_topic(self, inter: disnake.ApplicationCommandInteraction):
+        async with aiosqlite.connect(DB_PATH) as db:
+            async with db.execute('''
+                SELECT town_topic FROM towns
+                WHERE guild_id = ?
+            ''', (inter.guild_id,)) as cursor:
+                if town_topic := await cursor.fetchone():
+                    await inter.response.send_message(f"Описание вашего города:\n{town_topic[0]}")
+                else:
+                    await inter.send("ГОЙДАААА", ephemeral=True)
 def setup(bot: commands.Bot):
     bot.add_cog(CommonAdminCog(bot))
     bot.add_cog(CommonMemberCog(bot))
