0efbb2b6f293c3061c02cd5ca85e978c59893e44
MaksimArkhipenko <maximarkhipenko07@gmail.com>
2024-11-21 17:28:32 -0500

    Add embed classes for Success, Error, Info, and Footer

---
+++ cogs/embeds.py
@@ -0,0 +1,45 @@
+import disnake
+from disnake import Embed
+
+class Footer(Embed):
+    def __init__(
+            self,
+            title: str = "",
+            description: str = "",
+            footer_text: str = "",
+            **kwargs
+            ):
+        super().__init__(title=title, description=description, **kwargs)
+        self.set_footer(text=footer_text)
+
+class Success(Footer):
+    def __init__(
+            self,
+            title: str = "Success",
+            description: str = "",
+            footer_text: str = "",
+            **kwargs):
+        super().__init__(title=title, description=description, footer_text=footer_text, **kwargs)
+        self.color = disnake.Color.green()
+
+class Error(Footer):
+    def __init__(
+            self,
+            title: str = "Error",
+            description: str = "",
+            footer_text: str = "",
+            **kwargs
+            ):
+        super().__init__(title=title, description=description, footer_text=footer_text, **kwargs)
+        self.color = disnake.Color.red()
+
+class Info(Footer):
+    def __init__(
+            self,
+            title: str = "Information",
+            description: str = "",
+            footer_text: str = "",
+            **kwargs
+            ):
+        super().__init__(title=title, description=description, footer_text=footer_text, **kwargs)
+        self.color = disnake.Color.blue()
